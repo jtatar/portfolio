14b7b35b41ba0a0a498d01dbea65edd95c5cbd1b
Jakub Tatar <13kubek@gmail.com>
2020-04-27 18:19:00 +0200

    finishing navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Github, Linkedin } from '@icons-pack/react-simple-icons'
 import About from './Components/About/About'
 import Home from './Components/Home/Home'
 import Projects from './Components/Projects/Projects'
@@ -14,10 +15,12 @@ import {
   Link
 } from "react-router-dom";
 
+
 const initialState ={
   homeActive: true,
   technologiesActive: false,
-  projectsActive: false
+  projectsActive: false,
+  mainColor: 'rgb(190,66,66)'
 }
 
 class App extends React.Component {
@@ -27,12 +30,14 @@ class App extends React.Component {
     this.state = initialState;
   }
 
-  toggleNavBar = (home, technologies, projects) => {
+  toggleNavBar = (home, technologies, projects, color) => {
     this.setState({
       homeActive: home,
       technologiesActive: technologies,
-      projectsActive: projects
+      projectsActive: projects,
+      mainColor: color
     })
+    document.documentElement.style.setProperty('--main-color-background',color)
   }
 
   render(){
@@ -44,9 +49,17 @@ class App extends React.Component {
               <h2 className='logo'>Jakub Tatar.</h2>
             </div>
             <nav className='navbar'>
-                  <Link onClick={() => this.toggleNavBar(true,false,false)} className={this.state.homeActive ? 'border' : ''} to="/">Home</Link>
-                  <Link onClick={() => this.toggleNavBar(false,true,false)} className={this.state.technologiesActive ? 'border' : ''} to="/technologies">Technologies</Link>
-                  <Link onClick={() => this.toggleNavBar(false,false,true)} className={this.state.projectsActive ? 'border' : ''} to="/projects">Projects</Link>
+              <Link onClick={() => this.toggleNavBar(true,false,false,'rgb(190,66,66)')} className={this.state.homeActive ? 'border' : ''} to="/">Home</Link>
+              <Link onClick={() => this.toggleNavBar(false,true,false,'rgb(255,210,78)')} className={this.state.technologiesActive ? 'border' : ''} to="/technologies">Technologies</Link>
+              <Link onClick={() => this.toggleNavBar(false,false,true,'rgb(71,199,100)')} className={this.state.projectsActive ? 'border' : ''} to="/projects">Projects</Link>
+            </nav>
+            <nav className='socials' >
+              <a target="_blank" href='https://github.com/jtatar'>
+                <Github color="#ffffff" size={24}/>
+              </a>
+              <a target="_blank" href='https://www.linkedin.com/in/jakub-tatar-0643411a8/'>
+                <Linkedin color="#ffffff" size={24}/>
+              </a>
             </nav>
           </header>
 
