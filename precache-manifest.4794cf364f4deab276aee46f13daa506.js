94fdc6d21856d8f2c700ba3a0e9e105498e2c868
Jakub Tatar <13kubek@gmail.com>
2021-02-10 14:19:46 +0100

    Updates

---
+++ precache-manifest.4794cf364f4deab276aee46f13daa506.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387bb3271939423d7ea1eb674268524b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "eb9363744beecf4a5fc0",
+    "url": "/portfolio/static/css/main.4dd940cd.chunk.css"
+  },
+  {
+    "revision": "94d2d9f50007293b2f57",
+    "url": "/portfolio/static/js/2.5417319f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.5417319f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb9363744beecf4a5fc0",
+    "url": "/portfolio/static/js/main.b9c8f5b9.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "5973cc334747c7faaa7e70db568ea050",
+    "url": "/portfolio/static/media/bmz.5973cc33.png"
+  },
+  {
+    "revision": "48ed2f9c3f38527c2d1cdd0a61c352ca",
+    "url": "/portfolio/static/media/facerecognition.48ed2f9c.png"
+  },
+  {
+    "revision": "b53b38fdcc5a7b61f9516326e6c9ecb2",
+    "url": "/portfolio/static/media/front.b53b38fd.gif"
+  },
+  {
+    "revision": "44c3d9e68ba14fa9228e20b6aca0ce31",
+    "url": "/portfolio/static/media/game.44c3d9e6.jpg"
+  },
+  {
+    "revision": "bba8c03f865aa92d5bd187505f947213",
+    "url": "/portfolio/static/media/itsilesia.bba8c03f.png"
+  },
+  {
+    "revision": "c85b3d3c93d9447ac3aca18536cf5119",
+    "url": "/portfolio/static/media/jokes.c85b3d3c.png"
+  },
+  {
+    "revision": "3640846bf18a6e877c729f8bf32d625c",
+    "url": "/portfolio/static/media/python.3640846b.png"
+  },
+  {
+    "revision": "91dd57a88a87d079089ce844f5df56d1",
+    "url": "/portfolio/static/media/ticketing.91dd57a8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6e4e53b2f0b7945b7ff6921a27a8fd30.js"
+  "/portfolio/precache-manifest.4794cf364f4deab276aee46f13daa506.js"
 );
 
 self.addEventListener('message', (event) => {
